33af0806c41dc923afe0bd1496e3889789cc12c7
skullxcode <pranavthawait@gmail.com>
2026-06-30 01:57:26 +0530

    some changes

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   serverExternalPackages: ['firebase-admin', 'jwks-rsa', 'jose', 'pg'],
-  allowedDevOrigins: ['192.168.1.8', '10.153.76.134'],
 };
 
 export default nextConfig;
